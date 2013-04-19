abfb15b9c040e83780e833ce587141e33286bf20
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-04-19 21:34:33 +0530

    exercise 1

---
+++ exercises/js/sandbox.js
@@ -0,0 +1,21 @@
+//1.1
+$("div .module")
+
+//1.2
+$("#myListItem").hide()
+$("ul#myList li").eq(2)
+$("ul#myList li:nth-child(3)")
+
+$("#myListItem").hide() is the best way.
+
+//1.3
+$('label[for="q"]')
+
+//1.4
+$(":hidden").length
+
+//1.5
+$("img[alt]").length
+
+//1.6
+$("#fruits tbody tr:odd")
