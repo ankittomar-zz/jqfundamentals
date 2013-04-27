c33b93e105835e09d28908085733ee1fa70051c2
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-04-27 12:13:21 +0530

    Exercise 2 completed

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -19,3 +19,33 @@ $("img[alt]").length
 
 //1.6
 $("#fruits tbody tr:odd")
+
+
+//===========================Exercise 2=================================================
+
+//2.1
+
+$("img").each(function get_image_alt_attribute() {
+    console.log( 'alt attribute : ' + $(this).attr('alt'))   
+})
+
+
+//2.2
+
+$('input[name="q"]').parent('form').addClass('eureka')
+
+//2.3
+
+element = $('#myList .current')
+element.removeClass('current')
+element.next().addClass('current')
+
+//2.4
+
+$('#specials select').parent().next('li').children('input.input_submit')
+
+//2.5
+
+$('#slideshow li:first').addClass('current').siblings().addClass('disabled'
+
+
