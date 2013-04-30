c5f15402503953ccb827297a60d601994a8a692e
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-04-30 17:53:29 +0530

    added jquery 1.9.1 and changed index.html to use jquery 1.9.1.js, added exercise 5.1

---
+++ exercises/js/blog.js
@@ -0,0 +1,8 @@
+var $blog_element = $('#blog');
+var $heading = $blog_element.find('h3');
+$heading.on('click', function(event) {
+    event.preventDefault();
+    var $a = $(this).parent().siblings('li').find('p.excerpt').slideUp('slow');
+    $(this).siblings('p.excerpt').slideDown('slow');
+})
+
