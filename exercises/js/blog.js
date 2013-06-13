fbfb37b5101666014d0885dfd9caf89fa1ec8d8e
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-06-13 18:25:48 +0530

    added visible property while sliding up

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -2,7 +2,7 @@ var $blog_element = $('#blog');
 var $heading = $blog_element.find('h3');
 $heading.on('click', function(event) {
     event.preventDefault();
-    var $a = $(this).parent().siblings('li').find('p.excerpt').slideUp('slow');
+    var $a = $(this).parent().siblings('li').find('p.excerpt:visible').slideUp('slow');
     $(this).siblings('p.excerpt').slideDown('slow');
 })
 
