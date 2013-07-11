296d1b3924350e06eb4c874db7cb208c5f6840c5
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-07-11 19:20:18 +0530

    added code to bind click event and store div reference in division

--- exercises/js/load.js
+++ exercises/js/load.js
@@ -1,48 +1,50 @@
 var LoadExternalContent = function() {
-  this.init();
+    this.init();
 } 
 
 
 LoadExternalContent.prototype = {
 
-  init: function() {
-    headline_elements = $('div#blog h3');
-    this.appendDivAfterEveryBlogHeading();
-    this.bindClick();
+    init: function() {
+        headline_elements = $('div#blog h3');
+        this.appendDivAfterEveryBlogHeading();
+        this.bindClick();
+    },
 
-  },
+    appendDivAfterEveryBlogHeading: function() {
+        this.appendDivElement($(headline_elements));
+        this.setReferenceInData();
+    },
 
-  appendDivAfterEveryBlogHeading: function() {
-    this.appendDivElement($(headline_elements));
-    this.setReferenceInData();
-  },
+    appendDivElement: function(element) {
+        element.append('<div class="child_div"></div>');
+    },
 
-  appendDivElement: function(element) {
-    element.append('<div class="child_div"></div>');
-  },
+    setReferenceInData: function() {
+        $.each(headline_elements, function(index, value) {
+            var headline_child_div = $(headline_elements[index]).children('div');
+            $(headline_elements[index]).data($(headline_child_div));
+        });  
+    },
 
-  setReferenceInData: function() {
-    $.each(headline_elements, function(index, value) {
-      var headline_child_div = $(headline_elements[index]).children('div');
-      $(headline_elements[index]).data($(headline_child_div));
-    });  
+    loadContentInTargetDiv: function(element) {
+        //debugger;
+        $(element).children('div').load('/exercises/data/blog.html #post1', function () { 
+            alert('fuck off');
+        });
 
-  },
-
-  loadContentInTargetDiv: function(element) {
-    
-  }
+    },
 
 
-  bindClick: function() {
-    var class_object = this;
-    headline_elements.bind('click', function(event) {
-    event.preventDefault();
-    class_object.loadContentInTargetDiv($(this));
-    })
-  }
+    bindClick: function() {
+        var class_object = this;
+        headline_elements.bind('click', function(event) {
+        event.preventDefault();
+        class_object.loadContentInTargetDiv($(this));
+        });
+    }
 }
 
 $(function(){
-  new LoadExternalContent();
+    new LoadExternalContent();
 });
