bbbc67776af6bad7c2a7388cab6e9fb6e3d276c5
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-07-24 13:29:07 +0530

    grid displayed and unique value array generated

--- exercises/js/grid.js
+++ exercises/js/grid.js
@@ -1,9 +1,65 @@
 var ProductGrid = function (){
-         
+    this.init();         
 }
 
 ProductGrid.prototype = {
     
+    init: function() {
+        this.makeAjaxRequest();
+    },
+
+    makeAjaxRequest: function() {
+       // console.log('in makeAjaxRequest');
+        class_object = this;
+        $.ajax({
+            url: "http://dl.dropboxusercontent.com/u/628209/exercises/javascript/product.json",
+            type: 'get',
+            dataType: 'JSON',
+            success: function(data){
+                class_object.generateGrid(data);
+                class_object.attributeList(data);
+
+            }
+        })
+    },
+
+    generateGrid: function(grid_json_data) {
+        //console.log('in generateGrid');
+        $.each(grid_json_data, function(index, value) {
+            $('body').append('<img data-key="url" class="spacing"></img>');
+            var element = $('img');
+            var json_key = $(element).data().key;
+           // console.log(this[json_key]);
+            var image_url = "data/product_data/images/" + this[json_key];
+            $(element[index]).attr('src', image_url);
+        })
+    },
+
+    attributeList: function(grid_json_data) {
+        var brand = [];
+        var color = [];
+        var brand_key = "brand";
+        var color_key = "color";
+        $.each(grid_json_data, function(index, value) {
+            if($.inArray(this[brand_key], brand) == -1)
+            {
+                brand.push(this[brand_key]);
+            }
+            if($.inArray(this[color_key], color) == -1)
+            {
+                color.push(this[color_key]);
+            }
+        })
+        class_object.generateFilterList(brand);
+        class_object.generateFilterList(color);
+    },
+
+    generatefilterList: function(element_array) {
+        $.each(element_array, function(index, value) {
+            
+        })
+    } 
+
 }
 
 $(function(){
