07d4d21c1da0403832cd5598aaad328193b3a437
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-07-27 02:40:40 +0530

    comleted grid exercise

--- exercises/js/grid.js
+++ exercises/js/grid.js
@@ -36,11 +36,14 @@ ProductGrid.prototype = {
     attributeList: function() {
         var brand = [];
         var color = [];
+        var sold_out = [];
         var brand_key = "brand";
         var color_key = "color";
+        var sold_out_key = "sold_out";
         $.each(grid_json_data, function(index, value) {
-            var color_element = '<input type="checkbox" class="checkbox" value="'+this[color_key]+'" data-color='+this[color_key]+' data-key='+color_key+'>'+this[color_key]+'</input>'+'</br>'
+            var color_element = '<input type="checkbox" class="checkbox" value="'+this[color_key]+'" data-color='+this[color_key]+' data-key='+color_key+'>'+this[color_key]+'</input>'+'</br>';
             var brand_element = '<input type="checkbox" class="checkbox" value="'+this[brand_key]+'" data-brand="'+this[brand_key]+'" data-key='+brand_key+'>'+this[brand_key]+'</input>'+'</br>';
+            var sold_out_element = '<input type="checkbox" class="checkbox" value="'+this[sold_out]+'" data-sold_out ="'+this[sold_out_key]+'" data-key='+sold_out_key+'>'+this[sold_out_key]+'</input>'+'</br>';
             if($.inArray(brand_element, brand) == -1)
             {
                 brand.push(brand_element);
@@ -49,12 +52,17 @@ ProductGrid.prototype = {
             {
                 color.push(color_element);
             }
+            if($.inArray(sold_out_element, sold_out) == -1)
+            {
+                sold_out.push(sold_out_element);
+            }
         })
         color.sort();
         brand.sort();
 
         class_object.generateFilterList(brand, brand_key);
         class_object.generateFilterList(color, color_key);
+        class_object.generateFilterList(sold_out, sold_out_key);
         class_object.bindEvent();
     },
     
@@ -67,14 +75,10 @@ ProductGrid.prototype = {
             $(item_class).append(val)            
         })
     },
-        
-        
-        
-
+    
     bindEvent: function() {
-        //console.log('bindEvent');
+        
         $('.checkbox').bind('change', function(){
-            // console.log(this);
             class_object.showSelectedItems($(this));
         })
     },
@@ -112,42 +116,104 @@ ProductGrid.prototype = {
     generateSearchString: function(filter_with_max_selection) {
         var max_selector_siblings = $(filter_with_max_selection).siblings();
         var max_selection_check_box = $(filter_with_max_selection).children('input');
-        var search_block,key_value, search_string = "";
-        var search_array = []; 
+        var search_block,key_value, search_string, data = "";
+        var search_array = [];
+        var  arr1 = [];
+        var arr2 = []; 
         $.each(max_selection_check_box, function(index, value) {
             if(value.checked)
             {   key_value = $(value).data().key;
                 search_block = "[data-"+key_value+"='"+$(value).data()[key_value]+"']";
+                arr1.push(search_block);
+                search_block = "";
+                console.log(arr1);     
                 $.each(max_selector_siblings, function(ind, val){
                     var max_selection_sibling_child_box = $(val).children('input');
                     $.each(max_selection_sibling_child_box, function(i,v) {
                         if(v.checked)
-                        {   
-                            key_value = $(v).data().key;
-                            search_block = search_block + "[data-"+key_value+"='" + $(v).data()[key_value] + "']";                            
-                            console.log(search_block);
-                            search_array.push(search_block);
-                            console.log(search_array);
-                            search_block = "";
-                                                 
+                        {   console.log(v);
+                            $.each(arr1, function(inde, valu) {
+                                key_value = $(v).data().key;
+                                data = valu+"[data-"+key_value+"='" + $(v).data()[key_value] + "']";
+                                arr2.push(data)
+                                data = "";
+                                console.log('arr2 temp arr');
+                                console.log(arr2);
+                            })  
                         }
                     })
+                    if(arr2.length > 0)
+                    {   
+                        arr1 = [];
+                        arr1 = arr2;
+                        arr2 = [];
+                    }
                 })
-
+                search_array.push(arr1);
+                arr1 = [];
             }
-            search_string = search_array.join(',');
-            console.log(search_string);
-            search_string = "img"+search_string;
-            console.log(search_string);
+        }) 
+        console.log(search_array);
+        search_string = search_array.join(',');
+        if (search_string)
+        {
             $('img.spacing').hide();
             $(search_string).show();
-
-        })            
+        }
+        else
+        {
+            $('img.spacing').show();
+        }
     }
- 
-    
 } 
 
 $(function(){
     new ProductGrid();
 })
+
+//==============================Scrap Code==============================//
+
+// generateSearchString: function(filter_with_max_selection) {
+//         var max_selector_siblings = $(filter_with_max_selection).siblings();
+//         var max_selection_check_box = $(filter_with_max_selection).children('input');
+//         var search_block,key_value, search_string = "";
+//         var flag = 0;
+//         var search_array = []; 
+//         $.each(max_selection_check_box, function(index, value) {
+//             if(value.checked)
+//             {   //flag = 0;
+//                 key_value = $(value).data().key;
+//                 search_block,search_block_max = "[data-"+key_value+"='"+$(value).data()[key_value]+"']";
+//                 $.each(max_selector_siblings, function(ind, val){
+//                     var mxa_selection_sibling_child_box = $(val).children('input');
+//                     $.each(max_selection_sibling_child_box, function(i,v) {
+//                         if(v.checked)
+//                         {   
+//                             flag = 1;
+//                             key_value = $(v).data().key;
+//                             search_block = search_block + "[data-"+key_value+"='" + $(v).data()[key_value] + "']";                            
+//                             //console.log(search_block);
+//                             //search_array.push(search_block);
+//                             //console.log(search_array);
+//                             //search_block = "";
+//                         }
+//                         if(!flag && !!search_block_max) {
+//                             //console.log(search_block_max);
+//                             search_array.push(search_block_max);
+//                             //console.log(search_array);
+//                             search_block_max = "";
+//                         }
+//                     })
+//                 })
+
+//             }
+//             search_string = search_array.join(',');
+//             //console.log(search_string);
+//             search_string = "img"+search_string;
+//             console.log(search_string);
+//             $('img.spacing').hide();
+//             $(search_string).show();
+//             search_array, search_string = "" 
+
+//         })            
+//     }
