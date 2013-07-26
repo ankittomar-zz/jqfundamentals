5c26613829f29df95abf6a05ea51a99b4295aa46
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-07-26 18:44:09 +0530

    add

--- exercises/js/grid.js
+++ exercises/js/grid.js
@@ -1,90 +1,152 @@
 var ProductGrid = function (){
+    class_object = this;
+    this.grid_json_data = [];    
     this.init();     
-    this.class_object;    
 }
 
 ProductGrid.prototype = {
     
     init: function() {
         this.makeAjaxRequest();
-        this.bindEvent();
     },
 
     makeAjaxRequest: function() {
-        class_object = this;
         $.ajax({
             url: "http://dl.dropboxusercontent.com/u/628209/exercises/javascript/product.json",
             type: 'get',
             dataType: 'JSON',
             success: function(data){
-                class_object.generateGrid(data);
-                class_object.attributeList(data);
-
+                grid_json_data = data;
+                class_object.generateGrid();
+                class_object.attributeList();
             }
         })
     },
 
-    generateGrid: function(grid_json_data) {
+    generateGrid: function() {
         $.each(grid_json_data, function(index, value) {
-            debugger;
-            $('#parent').append('<div><img data-key="url" class="spacing '+this.color.toLowerCase()+' '+this.brand.replace(/\s/,'_').toLowerCase"></img>');
+            $('#parent').append('<img data-name='+this.name+' data-url='+this.url+' data-color='+this.color+' data-sold_out='+this.sold_out+' data-brand="'+this.brand+'" class="spacing"></img>');
             var element = $('img.spacing');
-            var json_key = $(element).data().key;
-            var image_url = "data/product_data/images/" + this[json_key];
+            var json_key = $(element).data().url;
+            var image_url = "data/product_data/images/" + this.url;
             $(element[index]).attr('src', image_url);
         })
     },
 
-    attributeList: function(grid_json_data) {
+    attributeList: function() {
         var brand = [];
         var color = [];
         var brand_key = "brand";
         var color_key = "color";
         $.each(grid_json_data, function(index, value) {
-            if($.inArray(this[brand_key], brand) == -1)
+            var color_element = '<input type="checkbox" class="checkbox" value="'+this[color_key]+'" data-color='+this[color_key]+' data-key='+color_key+'>'+this[color_key]+'</input>'+'</br>'
+            var brand_element = '<input type="checkbox" class="checkbox" value="'+this[brand_key]+'" data-brand="'+this[brand_key]+'" data-key='+brand_key+'>'+this[brand_key]+'</input>'+'</br>';
+            if($.inArray(brand_element, brand) == -1)
             {
-                brand.push(this[brand_key]);
-
+                brand.push(brand_element);
             }
-            if($.inArray(this[color_key], color) == -1)
+            if($.inArray(color_element, color) == -1)
             {
-                color.push(this[color_key]);
+                color.push(color_element);
             }
         })
+        color.sort();
+        brand.sort();
 
         class_object.generateFilterList(brand, brand_key);
         class_object.generateFilterList(color, color_key);
+        class_object.bindEvent();
     },
+    
 
     generateFilterList: function(element_array, key) {
         element = $('#parent_left');
+        $(element).append('<div class="td_left filter '+ key+'"/>');
+        var item_class = '.'+key;
         $.each(element_array, function(index, val) {
-            $(element).append('<input type="checkbox" class="checkbox" value='+val+' data-key='+key+'>'+val+'</input>'+'</br>');            
+            $(item_class).append(val)            
         })
-        $(element).append('<div class="td_left"/>');
-        class_object.bindEvent();
     },
+        
+        
+        
 
     bindEvent: function() {
-        class_object = this;
-        console.log("hiloio =");
+        //console.log('bindEvent');
         $('.checkbox').bind('change', function(){
-             
+            // console.log(this);
             class_object.showSelectedItems($(this));
-
         })
     },
 
     showSelectedItems: function(element) {
         
-        element.addClass('checked'); 
+        var selector = element.data().key;
+        var filter_div = $('.filter');
+        var filter_with_max_selection = class_object.findFilterWithMaxSelection(filter_div);
+        class_object.generateSearchString($(filter_with_max_selection));
     },
 
-    generateClassList: function(element){
-        $('checked')
-    }
-} 
+    findFilterWithMaxSelection: function(filter_div) {
+        var filter_with_max_selection = "";
+        var count, max_count =0;
+        $.each(filter_div, function(ind, val) {
+            var children = $(val).children('input');
+            count = 0;
+            $.each(children, function(index, value) {
+                if(value.checked) {
+                    count++;
+                    if (count > max_count) {
+                        max_count = count;
+                        filter_with_max_selection = $(value).parent();
+                    }
+                    else {
+                        //do nothing
+                    }
+                }
+            })
+        })
+        return filter_with_max_selection;    
+    },
 
+    generateSearchString: function(filter_with_max_selection) {
+        var max_selector_siblings = $(filter_with_max_selection).siblings();
+        var max_selection_check_box = $(filter_with_max_selection).children('input');
+        var search_block,key_value, search_string = "";
+        var search_array = []; 
+        $.each(max_selection_check_box, function(index, value) {
+            if(value.checked)
+            {   key_value = $(value).data().key;
+                search_block = "[data-"+key_value+"='"+$(value).data()[key_value]+"']";
+                $.each(max_selector_siblings, function(ind, val){
+                    var max_selection_sibling_child_box = $(val).children('input');
+                    $.each(max_selection_sibling_child_box, function(i,v) {
+                        if(v.checked)
+                        {   
+                            key_value = $(v).data().key;
+                            search_block = search_block + "[data-"+key_value+"='" + $(v).data()[key_value] + "']";                            
+                            console.log(search_block);
+                            search_array.push(search_block);
+                            console.log(search_array);
+                            search_block = "";
+                                                 
+                        }
+                    })
+                })
+
+            }
+            search_string = search_array.join(',');
+            console.log(search_string);
+            search_string = "img"+search_string;
+            console.log(search_string);
+            $('img.spacing').hide();
+            $(search_string).show();
+
+        })            
+    }
+ 
+    
+} 
 
 $(function(){
     new ProductGrid();
