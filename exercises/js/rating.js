0b21ed6dbdb298751365df1360c4913a125f04c3
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-08-23 19:27:13 +0530

    fixed rating.js

--- exercises/js/rating.js
+++ exercises/js/rating.js
@@ -5,14 +5,17 @@ var Rating = function() {
 Rating.prototype = {
 
   bindEvent: function() {
-    var that = this;
     $('td.rating,.beverage').bind('click', function(){ 
-      $(".select[data-group = "+$(this).data('group')"]").removeClass('select');
-      $(this).addClass('select');
-      if ($(this).hasClass('rating') && $('.beverage.select').length>0)
+      $(".select[data-group = "+$this.data('group')+"]").removeClass('select');
+      $this.addClass('select');
+      if ($this.hasClass('rating') && $('.beverage.select').length>0)
       {
-        $("input[name="+$('.beverage.select').data('name')+"][value="+$(this).data('name')+"]").prop({'checked':true, 'disabled':false});
+        $("input[name="+$('.beverage.select').data('name')+"][value="+$this.data('name')+"]").prop({'checked':true, 'disabled':false});
         $("input[type='radio']:not(:checked)").prop('disabled', true);
+      }
+      else
+      {
+        $('.rating').removeClass('select');
       }
     })
   }
