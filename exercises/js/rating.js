673d29ecfed591ab90b4013a1378130b5db9da8f
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-08-23 20:02:38 +0530

    fixed rating.js

--- exercises/js/rating.js
+++ exercises/js/rating.js
@@ -5,17 +5,18 @@ var Rating = function() {
 Rating.prototype = {
 
   bindEvent: function() {
-    $('td.rating,.beverage').bind('click', function(){ 
-      $(".select[data-group = "+$this.data('group')+"]").removeClass('select');
+    $('td.rating, td.beverage').bind('click', function(){ 
+      var $this = $(this);
+      $("td.select[data-group = " + $this.data('group') + "]").removeClass('select');
       $this.addClass('select');
-      if ($this.hasClass('rating') && $('.beverage.select').length>0)
+      if ($this.hasClass('rating') && $('.beverage.select').length > 0)
       {
-        $("input[name="+$('.beverage.select').data('name')+"][value="+$this.data('name')+"]").prop({'checked':true, 'disabled':false});
+        $("input[name=" + $('.beverage.select').data('name') + "][value=" + $this.data('name') + "]").prop({'checked':true, 'disabled':false});
         $("input[type='radio']:not(:checked)").prop('disabled', true);
       }
       else
       {
-        $('.rating').removeClass('select');
+        $('td.rating.select').removeClass('select');
       }
     })
   }
