52a4e22d6a0819ab4d7343e3f4ca0997e266e8b3
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-08-25 20:29:49 +0530

    optimised grid.js

--- exercises/js/grid.js
+++ exercises/js/grid.js
@@ -31,70 +31,57 @@ ProductGrid.prototype = {
     bindEvent: function() {
         var class_object = this;
         $('.checkbox').bind('change', function(){
-            class_object.showSelectedItems($(this));
+            class_object.filterData();
         })
     },
 
-    showSelectedItems: function(element) {
-        
-        var selector = element.data('key'), filter_div = $('.filter');
-        var filter_with_max_selection = this.findFilterWithMaxSelection(filter_div);
-        this.generateSearchString($(filter_with_max_selection));
-    },
-
-    findFilterWithMaxSelection: function(filter_div) {
+    findFilterWithMaxSelection: function() {
         var checked_count_arr = [];
-        return $(filter_div).each(function(i,e){ 
+        return $('.filter').each(function(i,e){ 
                 checked_count_arr.push($(e).find('input:checked').length) 
-            })[checked_count_arr.indexOf(Math.max.apply(Math,checked_count_arr))]
+        })[checked_count_arr.indexOf(Math.max.apply(Math,checked_count_arr))]
     },
 
-    generateSearchString: function(filter_with_max_selection) {
-        var max_selector_siblings = $(filter_with_max_selection).siblings();
-        var max_selection_check_box = $(filter_with_max_selection).children('input:checked');
-        var search_block,key_value, search_string, data = "";
-        var search_array = [], arr1 = [], arr2 = []; 
-        $.each(max_selection_check_box, function(index, value) {
-            key_value = $(value).data('key'); 
-            search_block = "[data-"+key_value+"='"+$(value).data(key_value)+"']";
+    filterData: function() {
+        var $filter_with_max_selection = $(this.findFilterWithMaxSelection()); 
+        var arr1 = [];
+        //generate search string with max selectors
+        $.each($filter_with_max_selection.children('input:checked'), function(index, value) {
+            var search_block = "";
+            var key_value = $(value).data('key'); 
+            search_block = "img[data-"+key_value+"='"+$(value).data(key_value)+"']";
             arr1.push(search_block); //push each checked element of selector with max selection in array
-            search_block = "";
-            //Iterate over all the sibling of selector with max selections
-            $.each(max_selector_siblings, function(index, value){
-                var max_selection_sibling_child_box = $(value).children('input:checked');
-                //Iterate over every child of Siblings of selector with max selection
-                $.each(max_selection_sibling_child_box, function(i,v) {
-                    //Iterate over the array to progressively generate the selector
-                    $.each(arr1, function(index, value) {
-                            key_value = $(v).data('key');
-                            data = value+"[data-"+key_value+"='" + $(v).data(key_value) + "']";
-                            arr2.push(data);// one complete selector pushed to Array3
-                            data = "";
-                        })  
-                })
-                if(arr2.length > 0)
-                {   
-                    arr1 = arr2;
-                    arr2 = [];
-                }
+        })
+        //Iterate over all the sibling of selector with max selections
+        $.each($filter_with_max_selection.siblings(), function(index, value){
+            var arr2 =[];           
+            //Iterate over every child of Siblings of selector with max selection
+            $.each($(value).children('input:checked'), function(i,v) {
+                //Iterate over the max selector array to progressively generate the selector
+                $.each(arr1, function(index, value) {
+                    var key_value = $(v).data('key');
+                    var data = value+"[data-"+key_value+"='" + $(v).data(key_value) + "']";
+                    arr2.push(data);// one complete selector pushed to Array2
+                })  
             })
-            search_array.push(arr1);
-            arr1 = [];
-        }) 
-        search_string = 'img' + search_array.join(',');
-
-        if (search_string)
+            if(!!arr2.length)
+            {   
+                arr1 = arr2;
+            }
+        })
+        
+     
+        var search_string = arr1.join(',')
+        if (!!arr1.length)
         {
-            $('img.spacing').hide();
-            $(search_string).show();
+            $('img.spacing').hide().filter(search_string).show();
         }
         else
         {
             $('img.spacing').show();
         }
-        console.log(search_string);
+        console.log(search_string);          
     }
-
 } 
 
 $(function(){
